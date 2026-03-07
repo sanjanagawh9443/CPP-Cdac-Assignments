0211f7162bc4370634b7a70b3039448c3a232d2d
Sanjana Gawhande <141925971+sanjanagawh9443@users.noreply.github.com>
2026-03-07 15:03:53 +0530

    Add files via upload

---
+++ constantptr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    class student
+    {
+        int rollno;
+        char name[30];
+
+        public:
+        void display();
+        
+        student (int,char*);
+    };
+
+    student::student(int r,char*,sptr)
+
+    {
+        rollNo = r;
+        strcpy ()
+    }
+}
